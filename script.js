588a59be1e845470b3487e898893ddd3d17ec0be
Sp-177 <shub404.x@gmail.com>
2024-05-14 00:44:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function onCalc(s){
+    const display=document.getElementById('result');
+    if(display.value!=='OFF'){
+    if(s=='AC')display.value='';
+    else display.value+=s;}
+}
+function deleteLast(){
+    
+    const display=document.getElementById('result');
+    if(display.value!=='OFF'){
+    display.value=display.value.slice(0,-1);}
+}
+function calc(){
+    const display=document.getElementById('result');
+    display.value=eval(display.value);
+}
+function turnOn(){
+    const display=document.getElementById('result');
+    if(display.value==='OFF'){
+        let time1=setTimeout(()=>{
+            display.value='ON';
+        },1000);
+    let time=setTimeout(()=>{
+        display.style.textAlign='right';
+        display.value='';
+        
+    },3000);
+    
+    
+    }
+    else{
+        display.style.textAlign='center';
+        display.value='OFF';
+    }
+}
